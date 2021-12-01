c73e48d7d6b3132fce75e21dd4ef5ec088e47951
Magnus <wiesel41@web.de>
2021-12-01 20:58:42 +0100

    update solution day 1
    created a memory friendly version, using generators

--- 01/solver.py
+++ 01/solver.py
@@ -1,57 +1,75 @@
-# input.txt contains the puzzle numbers
+## naive solutions ##
 
-def sonar_number_of_increase(inputpath: str) -> int:
-    """
-    :param inputpath: path to the file containing one reading per line
-    :returns: the number of increases between readings
-    """
-    readings = []
-
-    #read numbers and store them in order
-    with open(inputpath, 'r') as file1:
-        lines = file1.readlines()
-        for line in lines:
-            readings.append(int(line.strip()))
-
-    # calculate the number of increases by comparing each reading to the previous one
-    n_of_increases = 0
-    previous_reading = readings[0]
-    for reading in readings[1:]:
-        if reading>previous_reading:
-            n_of_increases +=1
-        previous_reading = reading
-
-    return n_of_increases
+def count_number_of_increases(valuelist: [int]) -> int:
+    # returns the number of times a value is higher than the previous one in the given list
+    increases = 0
+    for i in range(1, len(valuelist)) :
+        if valuelist[i] > valuelist[i-1] :
+            increases += 1
+    return increases
 
 
-def sonar_number_of_increase_sliding(inputpath: str) -> int:
-    """
-    like sonar_number_of_increase but considers a three-measurement sliding window
-    :param inputpath: path to the file containing one reading per line
-    :returns: the number of increases between window sums
-    """
-    readings = []
-    
-    #read numbers and store them in order
-    with open(inputpath, 'r') as file1:
-        lines = file1.readlines()
-        for line in lines:
-            readings.append(int(line.strip()))
+def parse_input_list(input_path: str) -> [int]:
+    values = []
+    with open(input_path, 'r') as file1:
+        for line in file1:
+            values.append(int(line.strip()))
+    return values
 
-    # calculate sliding window sums by creating a tupel for each window and summing those tupels
-    # for this just zip the elements with an offset 
-    # keep in mind that the resulting list has window lenght -1 entries less than the original one
-    window_sums = list(map(
-        lambda x: x[0]+x[1]+x[2],
-        zip(readings[0:-2],readings[1:-1],readings[2:])
-        ))
 
-    # calculate the number of increases by comparing each window sum to the previous one
-    n_of_increases = 0
-    previous_sum = window_sums[0]
-    for window in window_sums[1:]:
-        if window>previous_sum:
-            n_of_increases +=1
-        previous_sum = window
+def sonar(input_path: str) -> int:
+    readings = parse_input_list(input_path)
+    increases = count_number_of_increases(readings)
+    return increases
 
-    return n_of_increases
+
+def sonar_sliding(input_path: str) -> int:
+    readings = parse_input_list(input_path)
+    window_sums = list( map( lambda x: sum(x), zip(readings[0:-2], readings[1:-1], readings[2:])))
+    increases = count_number_of_increases(window_sums)
+    return increases
+
+
+## solutions using generators to be more memory friendly ##
+
+def input_list_generator(input_path):
+    for line in open(input_path, 'r'):
+        yield int(line.strip())
+
+
+def count_number_of_increases_from_generator(valuegen) -> int:
+    # returns the number of times a value is higher than the previous one in the given generator
+    # if the genrator does not stop, neither will this function
+    increases = 0
+    previous_value = next(valuegen)
+    for value in valuegen :
+        if value > previous_value :
+            increases += 1
+        previous_value = value
+    return increases
+
+
+def sonar2(input_path: str) -> int:
+    gen = input_list_generator(input_path)
+    increases = count_number_of_increases_from_generator(gen)
+    return increases
+
+
+def sonar_sliding2(input_path: str) -> int:
+    def gen_window(valuegen):
+        x = next(valuegen)
+        y = next(valuegen)
+        for z in valuegen:
+            yield x + y + z
+            x = y
+            y = z
+
+    gen = input_list_generator(input_path)
+    # use the generator to create a new one that yields the sums of the sliding window
+    window_gen = gen_window(gen)
+    increases = count_number_of_increases_from_generator(window_gen)
+    return increases
+
+
+#print("solution 1: ", sonar2("input.txt"))
+#print("solution 2: ", sonar_sliding2("input.txt"))
