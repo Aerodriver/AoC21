92eea037f2da49caccf9a35fde80e183b939e0f9
Magnus <wiesel41@web.de>
2021-12-02 21:37:58 +0100

    update the solution for the second part
    It is so much worse tha before.
    One can change lists via lambda expressions and even concatenate multiple operations via "something if action1 or action2 or action3 else whatever". And because the lists variable is given as a default value at the creation of the dictionary, we can change its entries in the loop without  needing to pass or overwrite it explicitly later on.

--- 02/solver.py
+++ 02/solver.py
@@ -3,17 +3,17 @@
 def command_generator(input_path):
     for line in open(input_path, 'r'):
         values = line.split()
-        yield {"name":str(values[0]).lower(), "value":int(values[1])}
+        yield {"name":str(values[0])[0], "value":int(values[1])}
 
 
 def submarine_mover1(input_path):
     #this is really not ideal, I just wanted to play around with dictionaries and lambda
     commands = { 
-    "forward": lambda h_d, value : (h_d[0]+value, h_d[1]), 
-    "up": lambda h_d, value : (h_d[0], h_d[1]-value),
-    "down": lambda h_d, value : (h_d[0], h_d[1]+value)
+    "f": lambda h_d, value : (h_d[0]+value, h_d[1]), 
+    "u": lambda h_d, value : (h_d[0], h_d[1]-value),
+    "d": lambda h_d, value : (h_d[0], h_d[1]+value)
     }
-    h_d= (0,0) # horizontal, depth
+    h_d = (0,0) # horizontal, depth
     command_gen = command_generator(input_path)
     for command in command_gen:
         h_d = commands[command["name"]](h_d, command["value"])
@@ -21,16 +21,20 @@ def submarine_mover1(input_path):
 
 
 def submarine_mover2(input_path):
-    commands = { 
-    "forward": lambda h, d, a, value : (h+value, d+(a*value), a), 
-    "up": lambda h, d, a, value : (h, d, a-value),
-    "down": lambda h, d, a, value : (h, d, a + value )
-    }
-    h_d_a= (0,0,0) # horizontal, depth, aim
-    command_gen = command_generator(input_path)
-    for command in command_gen:
-        h_d_a = commands[command["name"]](*h_d_a , command["value"])
-    return(h_d_a[0]*h_d_a[1])
+    h_d_a = [(0,0,0)] # horizontal, depth, aim
+    # switch-case buts its just dicitonaries and lambdas
+    funcs = { 
+            "f": lambda value : 
+                1 if (h_d_a.append((h_d_a[-1][0]+value, h_d_a[-1][1]+(h_d_a[-1][2]*value), h_d_a[-1][2])) or h_d_a.pop(0)) else 0, 
+            "u": lambda value: 
+                1 if (h_d_a.append((h_d_a[-1][0], h_d_a[-1][1], h_d_a[-1][2]-value)) or h_d_a.pop(0)) else 0,
+            "d": lambda value : 
+                1 if (h_d_a.append((h_d_a[-1][0], h_d_a[-1][1], h_d_a[-1][2] + value)) or h_d_a.pop(0)) else 0,
+        }
+    commands = ({"n":str(ls[0])[0], "v":int(ls[1])} for s in open(input_path, 'r') for ls in [s.split()])
+    for command in commands:
+        funcs[command["n"]](command["v"])
+    return (h_d_a[0][0]*h_d_a[0][1])
 
 
-# print(submarine_mover2("input.txt"))
+print(submarine_mover2("input.txt"))
