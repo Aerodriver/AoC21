260c08cbf1cb8dc3b47bca6d251971399a0da23a
Magnus <wiesel41@web.de>
2021-12-02 22:57:58 +0100

    Update solver.py
    use deque with maxlen =0 to consume the generator more easily

--- 02/solver.py
+++ 02/solver.py
@@ -21,8 +21,9 @@ def submarine_mover1(input_path):
 
 
 def submarine_mover2(input_path):
-    h_d_a = [(0,0,0)] # horizontal, depth, aim
-    # switch-case buts its just dicitonaries and lambdas
+    from collections import deque
+    h_d_a = [(0,0,0)]
+    mult = lambda x: x[0]*x[1]
     funcs = { 
             "f": lambda value : 
                 1 if (h_d_a.append((h_d_a[-1][0]+value, h_d_a[-1][1]+(h_d_a[-1][2]*value), h_d_a[-1][2])) or h_d_a.pop(0)) else 0, 
@@ -30,9 +31,8 @@ def submarine_mover2(input_path):
                 1 if (h_d_a.append((h_d_a[-1][0], h_d_a[-1][1], h_d_a[-1][2]-value)) or h_d_a.pop(0)) else 0,
             "d": lambda value : funcs["u"](value*-1),
             }
-    if 0 in (funcs[v["n"]](v["v"]) for v in ({"n":str(ls[0])[0], "v":int(ls[1])} for s in open(input_path, 'r') for ls in [s.split()]) ):
-        return None
-    return (h_d_a[0][0]*h_d_a[0][1])
+    deque((funcs[c["n"]](c["v"]) for c in ({"n":str(ls[0])[0], "v":int(ls[1])} for s in open(input_path, 'r') for ls in [s.split()])), maxlen=0)
+    return (mult(h_d_a.pop()))
 
 
 print(submarine_mover2("input.txt"))
