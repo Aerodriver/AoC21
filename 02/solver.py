52b3633c325013757b09151da322ce806479ffba
Magnus <wiesel41@web.de>
2021-12-03 13:34:37 +0100

    Update solver.py
    do not use deque in order to have no imports

--- 02/solver.py
+++ 02/solver.py
@@ -21,7 +21,6 @@ def submarine_mover1(input_path):
 
 
 def submarine_mover2(input_path):
-    from collections import deque
     h_d_a = [(0,0,0)]
     mult = lambda x: x[0]*x[1]
     funcs = { 
@@ -31,8 +30,8 @@ def submarine_mover2(input_path):
                 1 if (h_d_a.append((h_d_a[-1][0], h_d_a[-1][1], h_d_a[-1][2]-value)) or h_d_a.pop(0)) else 0,
             "d": lambda value : funcs["u"](value*-1),
             }
-    deque((funcs[c["n"]](c["v"]) for c in ({"n":str(ls[0])[0], "v":int(ls[1])} for s in open(input_path, 'r') for ls in [s.split()])), maxlen=0)
-    return (mult(h_d_a.pop()))
+    if not 0 in (funcs[c["n"]](c["v"]) for c in ({"n":str(ls[0])[0], "v":int(ls[1])} for s in open(input_path, 'r') for ls in [s.split()])) :
+        return (mult(h_d_a.pop()))
 
 
 print(submarine_mover2("input.txt"))
