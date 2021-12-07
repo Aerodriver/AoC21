54b7e8f76cdbc02a7c76d678ae84783c01386bde
Magnus <wiesel41@web.de>
2021-12-07 17:29:36 +0100

    Update solver.py
    minor adjustments on when to stop calculating the fuel_costs per position

--- 07/solver.py
+++ 07/solver.py
@@ -2,38 +2,49 @@
 # find minimal position change:
 
 def crab_pos(input_path):
-    crab_list = []
-    with open(input_path, "r") as file:
-        for x in file.readline().split(","):
-            crab_list.append(int(x))
-    fuel_best = len(crab_list)*len(crab_list)
-    for i in range(len(crab_list)):
-        fuel_temp = 0
-        for c in crab_list:
-            crab_distance = abs(c-i)
-            fuel_temp = (fuel_temp + crab_distance)
-        if fuel_temp < fuel_best:
-            fuel_best = fuel_temp
-
-    return fuel_best
-
-
-def crab_pos2(input_path):
+    # one can also use the median
     crab_list = []
     with open(input_path, "r") as file:
         for x in file.readline().split(","):
             crab_list.append(int(x))
     fuel_best = None
-    for i in range(len(crab_list)):
+    #best_pos = 0
+    for i in range(max(crab_list)):
         fuel_temp = 0
         for c in crab_list:
             crab_distance = abs(c-i)
-            nedded_fuel = ((abs(c-i))**2 + abs(c-i))/2
+            fuel_temp = (fuel_temp + crab_distance)
+            if i > 1 and fuel_temp > fuel_best:
+                continue
+        if  not fuel_best or fuel_temp <  fuel_best:
+            fuel_best = fuel_temp
+            #best_pos = i
+    #print(best_pos,fuel_best)
+    return fuel_best
+
+
+def crab_pos2(input_path):
+     # one can also use the average/mean
+    crab_list = []
+    with open(input_path, "r") as file:
+        for x in file.readline().split(","):
+            crab_list.append(int(x))
+    #best_pos = 0
+    fuel_best = None
+    for i in range(max(crab_list)):
+        fuel_temp = 0
+        for c in crab_list:
+            crab_distance = abs(c-i)
+            nedded_fuel = (crab_distance**2 + crab_distance)/2
             fuel_temp = (fuel_temp + nedded_fuel)
+            if i > 1 and fuel_temp > fuel_best:
+                continue
         if not fuel_best or fuel_temp < fuel_best:
             fuel_best = fuel_temp
-
+            #best_pos = i
+    #print(best_pos, fuel_best)
     return fuel_best
+
 
 print(crab_pos("input.txt"))
 print(crab_pos2("input.txt"))
